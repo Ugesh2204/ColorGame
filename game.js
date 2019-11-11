072e6506174f501555ba36bdec6e8641290b92e5
ugesh <ugesh.roopchand@hotmail.com>
2019-11-11 10:04:59 +0400

    Simple click comparison

---
+++ game.js
@@ -0,0 +1,34 @@
+//RGB colors list
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)",
+]
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+
+var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent= pickedColor;
+
+for(var i = 0; i < squares.length; i++) {
+    //add initial colors to square
+    squares[i].style.backgroundColor = colors[i];
+
+    //add click listeners to square
+    squares[i].addEventListener("click", function(){
+        //grab color of clicked sqaure
+        var clickedColor = this.style.backgroundColor;
+        //compare color to pickedColor
+        if(clickedColor === pickedColor) {
+            alert("correct");
+        }else {
+            alert("Wrong !!!");
+        }
+    })
+}
+
+
