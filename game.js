95e58bc3f7462fa0f4319f21ab76fbba0ea62aaa
ugesh <ugesh.roopchand@hotmail.com>
2019-11-11 11:07:13 +0400

    Function generate Ramdom RGB

--- game.js
+++ game.js
@@ -1,18 +1,14 @@
 //RGB colors list
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-]
+var colors = generateRandomColors(6);
+
+
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 colorDisplay.textContent= pickedColor;
+var h1 = document.querySelector("h1");
 
 for(var i = 0; i < squares.length; i++) {
     //add initial colors to square
@@ -27,6 +23,7 @@ for(var i = 0; i < squares.length; i++) {
             messageDisplay.textContent = "Correct";
             //function call changeColors
             changeColors(clickedColor);
+            h1.style.background = clickedColor;
         }else {
            this.style.backgroundColor ="#232323";
            messageDisplay.textContent = "Try Again";
@@ -49,5 +46,32 @@ function pickColor(){
     //random color 0- 1
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+
+//function generate ramdom 
+
+function generateRandomColors(num){
+    //make an array
+    var arr = [];
+    //repeat num times
+     for(var i = 0; i < num; i++){
+      //get random color and push into array
+      arr.push(randomColor());
+     }
+    //return that array
+    return arr;
+
+}
+
+
+function randomColor(){
+
+    //pixk a "red" from 0 -255
+    var r = Math.floor(Math.random() * 256);
+    //Pick a "green" from 0-255
+    var g = Math.floor(Math.random() * 256);
+    //pick a "blue" from 0-255
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g +", " + b +")";
 }
 
